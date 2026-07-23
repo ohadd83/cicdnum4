7dd6ce0070912d786cd63099063b6a67316479ab
ohadd83 <ohad306@gmail.com>
2026-07-23 08:51:00 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 http.createServer((req, res) => {
-  res.end("Hello Jenkins CI/CD lets move on aaaaaa port 8081 23_7_26");
+  const now = new Date();
+  res.end("Hello Jenkins CI/CD lets move on aaaaaa port 8081 date is: ${now}");
 }).listen(3000);
 
