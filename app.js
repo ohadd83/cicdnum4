7fffc1b57fb003ea76eccc7f189fbcf77ac2ba18
ohadd83 <ohad306@gmail.com>
2026-07-23 10:25:58 +0000

    update

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 http.createServer((req, res) => {
   const now = new Date();
-  res.end(`Hello Jenkins CI/CD lets move on and test the webhook after add / - port 8081 date is: - ${now}`);
+  res.end(`Hello Jenkins CI/CD lets move on and test the webhook try number 2 -port 8081 date is: - ${now}`);
 }).listen(3000);
 
